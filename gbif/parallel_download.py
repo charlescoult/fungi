8a2170262b35703a2a1e6a8798e821a68605bc68
Charles Coulter <charlescoult.wrk@gmail.com>
2023-01-21 07:36:23 -0800

    filtering dataset and fixing download

--- gbif/parallel_download.py
+++ gbif/parallel_download.py
@@ -7,27 +7,28 @@ from urllib.parse import urlparse
 import tensorflow as tf
 
 def download_image( args ):
-    print(args)
-
+    
     # parse arguments
     url = args[0]
-    if (len(args) == 1):
-        fn = os.path.basename( urlparse(url).path )
-        ext = ''
-    else:
-        fn = args[1]
-        ext = os.path.splitext( urlparse(url).path )[1]
+    index = args[1]
+    ext = args[2]
+    fn = os.path.join('/mnt/media', str( index ) ) + ext
+
+    # return if file already exists
+    if (os.path.exists(fn)):
+        print(f'File "{fn}" already exists')
+        return
 
     # submit request and get response
     try:
         r = requests.get(url)
     except Exception as e:
         print('Exception in request:', e)
-
+        return index
+    
     image = r.content
 
-    print( url )
-
+    '''
     # resize image
     ## decode image to tf.image
     image = tf.io.decode_image( image )
@@ -40,24 +41,35 @@ def download_image( args ):
             # antialias=False,
             # name=None,
             )
+    '''
 
     ## Convert back to raw
-    image = tf.io.encode_jpeg( image ).numpy()
+    # image = tf.io.encode_jpeg( image ).numpy()
 
     # os.makedirs(os.path.dirname('media'), exist_ok=True)
-
+    
     try:
-        with open(os.path.join( 'media', str(fn) + ext ), 'wb') as f:
+        with open( fn , 'wb' ) as f:
             f.write(image)
     except Exception as e:
-        print('Exception in saving image:', e)
+        print('Exception in saving file:', e)
+        return index
 
 def download_parallel( args ):
     cpus = cpu_count()
     results = ThreadPool(cpus - 1).imap_unordered( download_image, args, chunksize=4 )
+    errors = []
     for result in results:
-        print( 'Result: ' + str(result) )
+        if (result):
+            with open('errors.txt','a') as f:
+                f.write(f'{result}\n')
 
 def download_sequential( args ):
     for arg in args:
         download_image( arg )
+
+def download( args, parallel = False ):
+    if (parallel):
+        download_parallel( args )
+    else:
+        download_sequential( args )
---
+++ gbif/test_download.py
@@ -0,0 +1,9 @@
+from parallel_download import download
+
+
+
+download( [(
+    'https://mui.com/static/sponsors/octopus-light.svg',
+    0,
+    '.svg',
+)] )
